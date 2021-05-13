06af856408c6321cfac8c270aabb330cb4726f3d
Tsukasa Arima <pyar6329@gmail.com>
2021-05-13 10:52:40 +0000

    add redis connection pub sub

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ use chat_proto::prost_types::{FileDescriptorProto, FileDescriptorSet, Timestamp}
 // use chat_proto::tonic::futures_core;
 use chat_proto::tonic::{transport::Server, Code, Request, Response, Status};
 use chat_proto::tonic_reflection::server::Builder;
+use redis::aio::Connection as RedisConnection;
+use redis::aio::PubSub as RedisPubSub;
+use redis::IntoConnectionInfo as RedisConnectionInfo;
+use redis::RedisError;
 use redis::{AsyncCommands, ControlFlow, PubSubCommands};
 use std::env;
 use std::sync::Arc;
 use std::time::SystemTime;
-use tokio::sync::mpsc::Sender;
-use tokio::sync::{mpsc, Mutex};
+use tokio::sync::{mpsc, oneshot, Mutex};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::StreamExt;
+// use tokio_stream::wrappers::ReceiverStream;
+// use tokio_stream::{self, StreamExt};
 
 // #[derive(Debug, StructOpt)]
 // pub struct ServerOptions {
@@ -43,42 +48,32 @@ impl Chat for MyChat {
 
     type JoinRoomStream = ReceiverStream<Result<JoinChatRoomResponse, Status>>;
 
-    async fn join_room(
-        &self,
-        requeston: Request<()>,
-    ) -> Result<Response<Self::JoinRoomStream>, Status> {
-        let msg = JoinChatRoomResponse {
-            id: 1,
-            message: "aaa".to_string(),
-            name: "bbb".to_string(),
-            date: Some(Timestamp::from(SystemTime::now())),
-        };
-        println!("aaaaa");
-
-        let (mut tx, mut rx) = mpsc::channel(4);
+    async fn join_room(&self, _: Request<()>) -> Result<Response<Self::JoinRoomStream>, Status> {
+        let (tx, rx) = mpsc::channel(4);
         let channel_name = "foo".to_string();
-        let conn_pubsub_clone = self.redis_conn_pubsub.clone();
+        // spawnは非同期にしないとOkで値を返せないので、awaitしないこと
         tokio::spawn(async move {
-            let mut conn_pubsub = conn_pubsub_clone.lock().await;
+            // 1 request毎にredis pub/subのconnectionを貼る
+            let mut conn_pubsub = connect_pubsub().await.unwrap();
             let _ = conn_pubsub.subscribe(channel_name).await;
             let mut stream = conn_pubsub.on_message();
+            // 非同期でloopしないと、returnで値を返せない
             while let Some(msg) = stream.next().await {
                 let payload: String = msg.get_payload().unwrap();
                 println!("channel '{}': {}", msg.get_channel_name(), payload);
-                tx.send(Ok(JoinChatRoomResponse {
+                let send_msg = Ok(JoinChatRoomResponse {
                     id: 1,
                     message: payload,
                     name: msg.get_channel_name().to_string(),
                     date: Some(Timestamp::from(SystemTime::now())),
-                }))
-                .await
-                .unwrap();
+                });
+                // 送信失敗したら非同期処理を終わる
+                if let Err(_) = tx.send(send_msg).await {
+                    println!("send was failed");
+                    break;
+                }
                 println!("send finished");
             }
-            // while let Some(receive_msg) = rx.recv().await {
-            //     println!("{:?}", receive_msg); // 結果を受け取る
-            // }
-            // return Ok(Response::new(ReceiverStream::new(rx)));
         });
         // 返す値はstreamのtransaction
         Ok(Response::new(ReceiverStream::new(rx)))
@@ -163,6 +158,64 @@ async fn connect() -> redis::RedisResult<redis::aio::Connection> {
         .get_async_connection()
         .await
 }
+
+async fn connect_pubsub() -> Result<RedisPubSub, RedisError> {
+    let redis_host_name =
+        env::var("REDIS_HOSTNAME").expect("missing environment variable REDIS_HOSTNAME");
+    let redis_password =
+        env::var("REDIS_PASSWORD").expect("missing environment variable REDIS_PASSWORD");
+    // redisのTLS接続はredissでOK
+    // https://github.com/lettuce-io/lettuce-core/wiki/Redis-URI-and-connection-details
+    let redis_conn_url = format!("redis://:{}@{}", redis_password, redis_host_name);
+
+    let client = redis::Client::open(redis_conn_url)?;
+    let conn = client.get_async_connection().await?;
+    Ok(conn.into_pubsub())
+}
+
+// #[derive(Clone)]
+// pub struct PubSubConnectionPool {
+//     conn: RedisPubSub,
+// }
+//
+// impl PubSubConnectionPool {
+//     // async fn new<T: RedisConnectionInfo>(params: T) -> Result<RedisPubSub, RedisError> {
+//     async fn new() -> Result<PubSubConnectionPool, RedisError> {
+//         let redis_host_name =
+//             env::var("REDIS_HOSTNAME").expect("missing environment variable REDIS_HOSTNAME");
+//         let redis_password =
+//             env::var("REDIS_PASSWORD").expect("missing environment variable REDIS_PASSWORD");
+//         // redisのTLS接続はredissでOK
+//         // https://github.com/lettuce-io/lettuce-core/wiki/Redis-URI-and-connection-details
+//         let redis_conn_url = format!("redis://:{}@{}", redis_password, redis_host_name);
+//
+//         let client = redis::Client::open(redis_conn_url)?;
+//         let conn = client.get_async_connection().await?;
+//         Ok(PubSubConnectionPool {
+//             conn: conn.into_pubsub(),
+//         })
+//     }
+// }
+
+// #[derive(Clone)]
+// pub struct ConnectionPool {
+//     conn: RedisConnection,
+// }
+//
+// impl ConnectionPool {
+//     fn new(conn: ConnectionManager) -> ConnectionPool {
+//         ConnectionPool { conn }
+//     }
+//
+//     pub async fn get(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>, RedisError> {
+//         self.conn.get(key).await
+//     }
+//
+//     pub async fn set(&mut self, key: &[u8], value: &[u8]) -> Result<(), RedisError> {
+//         self.conn.as_pubsub
+//         // self.conn.subscribe("foo".to_string()).await
+//     }
+// }
 
 fn connect_normal() -> redis::RedisResult<redis::Connection> {
     let redis_host_name =
